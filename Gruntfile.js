6e855bfc89c7bbae92af7335fb495c9127453384
Eden S.-G <edgro2693@gmail.com>
2014-09-03 18:07:12 -0400

    add grunt-autoprefixer, remove prefixes
    grunt-autoprefixer (see https://github.com/postcss/autoprefixer)
    automatically prefixes css based on caniuse data. I added a grunt task
    to do this, and removed the prefixes from the LESS.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,37 +1,12 @@
 module.exports = function(grunt) {
     grunt.initConfig({
-        uglify: {
-            js: {
-                files: {
-                    'widget.min.js': ['widget.js'],
-                    'iframe/js/min/modal.min.js': ['iframe/js/modal.js'],
-                    'iframe/js/min/banner.min.js': ['iframe/js/banner.js'],
-                    'iframe/js/min/common.min.js': ['iframe/js/common.js']
-                }
-            }
-        },
-        less: {
-            development: {
-                options: {
-                    compress: true,
-                    cleancss: true,
-                    yuicompress: true,
-                    optimization: 2
-                },
-                files: {
-                    // target.css file: source.less file
-                    "iframe/css/min/modal.min.css": "iframe/css/modal.less",
-                    "iframe/css/min/banner.min.css": "iframe/css/banner.less"
-                }
-            }
-        },
         watch: {
             styles: {
                 files: [
                     'iframe/css/*.less',
                 ], // which files to watch
                 tasks: [
-                    'less'
+                    'less', 'autoprefixer'
                 ],
                 options: {
                     spawn: false,
@@ -59,6 +34,40 @@ module.exports = function(grunt) {
                     reload: false
                 }
             }
+        },
+        uglify: {
+            js: {
+                files: {
+                    'widget.min.js': ['widget.js'],
+                    'iframe/js/min/modal.min.js': ['iframe/js/modal.js'],
+                    'iframe/js/min/banner.min.js': ['iframe/js/banner.js'],
+                    'iframe/js/min/common.min.js': ['iframe/js/common.js']
+                }
+            }
+        },
+        less: {
+            development: {
+                options: {
+                    cleancss: true,
+                    compress: true,
+                    yuicompress: true,
+                    optimization: 2
+                },
+                files: {
+                    // target.css file: source.less file
+                    "iframe/css/min/modal.min.css": "iframe/css/modal.less",
+                    "iframe/css/min/banner.min.css": "iframe/css/banner.less"
+                }
+            }
+        },
+        autoprefixer: {
+            options: {
+                browsers: ['last 2 versions', 'ie >= 8', '> 1%', 'Firefox ESR', 'Opera 12.1']
+            },
+            css: {
+                // Target-specific file lists and/or options go here.
+                src: 'iframe/css/min/*.css'
+            },
         },
         browserSync: {
             dev: {
@@ -100,15 +109,16 @@ module.exports = function(grunt) {
     });
 
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-autoprefixer');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-browser-sync');
     grunt.loadNpmTasks('grunt-imageoptim');
     grunt.loadNpmTasks('grunt-newer');
 
-    grunt.registerTask('default', ['newer:uglify:js', 'newer:less:development', 'newer:imageoptim:images']);
-    
-    grunt.registerTask('build', ['uglify', 'less', 'imageoptim']);
+    grunt.registerTask('default', ['newer:uglify:js', 'newer:less:development', 'newer:autoprefixer:css', 'newer:imageoptim:images']);
+
+    grunt.registerTask('build', ['uglify', 'less', 'autoprefixer', 'imageoptim']);
     grunt.registerTask('watching', ['default', 'watch']);
     grunt.registerTask('serve', ['browserSync:dev', 'watching']);
     grunt.registerTask('host', ['browserSync:host']);
